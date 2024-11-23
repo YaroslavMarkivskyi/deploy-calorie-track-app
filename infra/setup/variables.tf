59a59299eb3b045ca8c51e9b5a6197a77dbe2a10
Yaroslav Markivskyi <yaroslav.markivskyi.it.2022@lpnu.ua>
2024-11-23 06:40:05 +0000

    delele: infra

--- infra/setup/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project" {
-  description = "Project name for tagging resources"
-  default     = "todo-app-api"
-}
-
-variable "contact" {
-  description = "Contact name for tagging resources"
-  default     = "yaroslav.markivskyi.it2022@lpnu.ua"
-}
